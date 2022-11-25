c18d61ecc6ac525e54a925fc1c462eda154e4113
deepakananth79 <deepakananth79@gmail.com>
2022-11-25 12:33:39 -0600

    new file:   pyword_dananth.py 	hall_of_fame.txt 	words.txt

---
+++ pyword_dananth.py
@@ -0,0 +1,48 @@
+"""
+Author: Deepak Ananthakrishnan, dananth@purdue.edu
+Assignment: 12.1 - pyword
+Date: 
+
+Description:
+    Program that runs a game similar to wordle in the terminal. Also has a "hall of fame" txt file that 
+    stores the highest scores from the game.
+
+
+Contributors:
+    Deepak, dananth@purdue.edu [repeat for each]
+
+My contributor(s) helped me:
+    [ ] understand the assignment expectations without
+        telling me how they will approach it.
+    [ ] understand different ways to think about a solution
+        without helping me plan my solution.
+    [ ] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor.
+
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
+
+"""Import additional modules below this line (starting with unit 6)."""
+
+
+"""Write new functions below this line (starting with unit 4)."""
+
+"""This function will take a list of all of the words in
+the words.txt file as its only argument and should return a list of three of those words chosen
+at random. The returned words must all be lowercase and must be used in the same order
+as they appear in the returned list """
+def pick_game_words():
+
+
+def main():
+    
+
+"""Do not change anything below this line."""
+if __name__ == "__main__":
+    main()
