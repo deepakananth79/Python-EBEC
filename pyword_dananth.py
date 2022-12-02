d79c34e25b29b4bfac62f165022d178891868272
deepakananth79 <deepakananth79@gmail.com>
2022-12-02 18:12:07 -0500

    modified:   pyword_dananth.py

--- pyword_dananth.py
+++ pyword_dananth.py
@@ -33,14 +33,67 @@ import random as r
 
 """Write new functions below this line (starting with unit 4)."""
 
-"""This function will take a list of all of the words in
-the words.txt file as its only argument and should return a list of three of those words chosen
-at random. The returned words must all be lowercase and must be used in the same order
-as they appear in the returned list """
-def pick_game_words():
+#Function that picks the words randomly, argument is a list of all the words
+def pick_game_words(words):
+    #List that stores the words from the word.txt file that were picked randomly
+    secret_words = []
+    i = 0
+    #Loop used to randomly select 3 words to use for the game
+    while(i < 3):
+        secret_words.append(words[r.randint(0, len(words) - 1)])
+        i += 1
+    return secret_words
+
+#Function that displays the main menu screen and redirects the user accordingly based off of the user input
+def main_menu():
+    print('----- Main Menu -----\n1. New Game\n2. See Hall of Fame\n3. Quit\n')
+    user_input = input('What would you like to do? ')
+    if(user_input == 1):
+        #List used to store all of the words from the word.txt file
+        words = []
+        with open("words.txt") as fo:
+            #For loop used to move each line from the word.txt file into a list
+            for line in fo: 
+                words.append(str(line.rstrip()))
+        pick_game_words(words)
+    elif(user_input == '2'):
+        print('\n--- Hall of Fame ---\n ## : Score : Player')
+        load_hall_of_fame()
+        for player,score in load_hall_of_fame().items():
+            print(f'{i:3} : {score:5s} : {player:s}')
+        print('')
+        main_menu()
+    elif(user_input == '3'):
+        print('Goodbye.')
+    else:
+        print('\nInvalid choice. Please try again.\n')
+        main_menu()
+
+#Function that returns the data from the hall_of_fame.txt file as a dictionary
+def load_hall_of_fame():
+
+    hall_of_fame_dict = {}
+
+    #Opens the hall_of_fame.txt file and splits up the data from the file into a dictionary
+    with open("hall_of_fame.txt") as fo:
+        for i in fo:
+            QandA = i.split(",") 
+            oldKey = QandA[1] 
+            key = oldKey[:-1].lstrip() 
+            value = QandA[0] 
+            hall_of_fame_dict[key] = value 
     
+    return hall_of_fame_dict
+
+
+#Function that runs the game itself
+def game():
+    player_name = input('Enter your player name: ')
+
 
 def main():
+    print('Welcome to PyWord.\n')
+    main_menu()
     
 
 """Do not change anything below this line."""
